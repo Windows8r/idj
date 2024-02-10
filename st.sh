e2c6cd06fbcade79687e3301b5769e6092993bc1
Windows8r <134237377+Windows8r@users.noreply.github.com>
2024-02-10 10:56:54 +0700

    Update st.sh

--- st.sh
+++ st.sh
@@ -22,6 +22,12 @@ sudo DEBIAN_FRONTEND=noninteractive \
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo systemctl disable lightdm.service
 echo Installing office (6/6)
-
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
+sudo apt update
+sudo apt install --install-recommends winehq-stable
 7z x msof.rar
 cd msof/Office2007SP2BlueEdition/
+wine setup.exe
